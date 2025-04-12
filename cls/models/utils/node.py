8cb243aef10212e88e531929035b16651a7d02ab
caelum_rog <18753896666@163.com>
2025-04-12 20:02:23 +0800

    add glif, klif

--- cls/models/utils/node.py
+++ cls/models/utils/node.py
@@ -282,7 +282,9 @@ class PSNTorch(nn.Module):
     https://arxiv.org/abs/2304.12760
 
     MUST set layer_by_layer = True
-    CANNOT use braincog BaseNode
+
+    CANNOT use braincog BaseNode. Because NOT feed timestep sequentially to neuron
+    Using nn.Module instead
     """
 
     def __init__(self,
@@ -292,7 +294,7 @@ class PSNTorch(nn.Module):
                  mem_detach=True,
                  act_func=AtanGrad):
         super().__init__()
-        assert layer_by_layer == True, "PSN MUST set layer_by_layer=True"
+        assert layer_by_layer is True, "PSN MUST set layer_by_layer=True"
         self.fc = nn.Linear(step, step)
         nn.init.constant_(self.fc.bias, -1)
 
@@ -305,3 +307,211 @@ class PSNTorch(nn.Module):
                             x_seq.flatten(1))  # [T,T] @ [T,*]
         spike = self.act_func(h_seq)
         return spike.view(x_seq.shape)
+
+
+class SpikeAct_extended(torch.autograd.Function):
+    '''
+    GLIF act function
+    solving the non-differentiable term of the Heavisde function
+    '''
+
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        # if input = u > Vth then output = 1
+        output = torch.gt(input, 0.)
+        return output.float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input = ctx.saved_tensors
+        grad_input = grad_output.clone()
+
+        # hu is an approximate func of df/du in linear formulation
+        hu = abs(input) < 0.5
+        hu = hu.float()
+
+        # arctan surrogate function
+        # hu =  1 / ((input * torch.pi) ** 2 + 1)
+
+        # triangles
+        # hu = (1 / gamma_SG) * (1 / gamma_SG) * ((gamma_SG - input.abs()).clamp(min=0))
+
+        return grad_input * hu
+
+
+class GLIFTorch(nn.Module):
+    '''
+        GLIF: A Unified Gated Leaky Integrate-and-Fire Neuron for Spiking Neural Networks
+        https://arxiv.org/abs/2210.13768
+
+        This may convert to BrainCog BaseNode version ?
+    '''
+
+    def __init__(self,
+                 step=10,
+                 soft_mode=False,
+                 static_gate=True,
+                 static_param=False,
+                 time_wise=True,
+                 layer_by_layer=True,
+                 gate=[0.6, 0.8, 0.6],
+                 param=[0.25, 0.5, -1, 0.5]):
+
+        # gate: [alpha, beta, gamma]
+        # param: [tau, Vth, linear_decay, conduct]
+
+        super().__init__()
+        self.T = step
+        self.soft_mode = soft_mode
+        self.static_gate = static_gate
+        self.static_param = static_param
+        self.time_wise = time_wise
+        self.gate = gate
+        self.param = param
+
+        assert layer_by_layer is True, "must set layer by layer=True for GLIFTorch"
+
+        # set linear_decay for param
+        linear_decay = param[1] / (
+            self.T * 2)  #linear decay coefficient, set to Vth/(T*2)
+        self.param[2] = linear_decay
+
+        self.alpha, self.beta, self.gamma = [
+            nn.Parameter(
+                torch.tensor(math.log(1 / ((i - 0.5) * 0.5 + 0.5) - 1),
+                             dtype=torch.float)) for i in self.gate
+        ]
+        if self.static_param:
+            self.tau, self.Vth, self.leak, self.conduct = [
+                torch.tensor(-math.log(1 / i - 1), dtype=torch.float)
+                for i in self.param
+            ]
+            self.reVth = self.Vth
+        else:
+            if self.time_wise:
+                self.tau, self.Vth, self.leak, self.conduct = [
+                    nn.Parameter(-math.log(1 / i - 1) *
+                                 torch.ones(self.T, dtype=torch.float))
+                    for i in self.param
+                ]
+                self.reVth = nn.Parameter(
+                    -math.log(1 / self.param[1] - 1) *
+                    torch.ones(self.T, dtype=torch.float))
+
+            else:
+                self.tau, self.Vth, self.leak = [
+                    nn.Parameter(
+                        torch.tensor(-math.log(1 / i - 1), dtype=torch.float))
+                    for i in self.param[:-1]
+                ]
+                self.reVth = nn.Parameter(
+                    torch.tensor(-math.log(1 / self.param[1] - 1),
+                                 dtype=torch.float))
+
+                self.conduct = [
+                    nn.Parameter(-math.log(1 / i - 1) *
+                                 torch.ones(self.T, dtype=torch.float))
+                    for i in self.param[3:]
+                ][0]
+
+    def forward(self, x):
+        u = torch.zeros(x.shape[1:], device=x.device)
+        out = torch.zeros(x.shape, device=x.device)
+        for step in range(self.T):
+            u, out[step] = self.extended_state_update(
+                u,
+                out[max(step - 1, 0)],
+                x[step],
+                tau=self.tau[step].sigmoid()
+                if self.time_wise else self.tau.sigmoid(),
+                Vth=self.Vth[step].sigmoid()
+                if self.time_wise else self.Vth.sigmoid(),
+                leak=self.leak[step].sigmoid()
+                if self.time_wise else self.leak.sigmoid(),
+                conduct=self.conduct[step].sigmoid()
+                if not self.static_param else self.conduct.sigmoid(),
+                reVth=self.reVth[step].sigmoid()
+                if self.time_wise else self.reVth.sigmoid())
+        return out
+
+    #
+    # def state_update(self, u_t_n1, o_t_n1, W_mul_o_t_n1, tau):
+    #     u_t1_n1 = tau * u_t_n1 * (1 - o_t_n1) + W_mul_o_t_n1
+    #     o_t1_n1 = spikeAct(u_t1_n1)
+    #     return u_t1_n1, o_t1_n1
+
+    def extended_state_update(self, u_t_n1, o_t_n1, W_mul_o_t_n1, tau, Vth,
+                              leak, conduct, reVth):
+        if self.static_gate:
+            al, be, ga = self.alpha.clone().detach().gt(
+                0.).float(), self.beta.clone().detach().gt(
+                    0.).float(), self.gamma.clone().detach().gt(0.).float()
+        else:
+            al, be, ga = self.alpha.sigmoid(), self.beta.sigmoid(
+            ), self.gamma.sigmoid()
+        # I_t1 = W_mul_o_t_n1 + be * I_t0 * self.conduct.sigmoid()#原先
+        I_t1 = W_mul_o_t_n1 * (1 - be * (1 - conduct))
+        u_t1_n1 = ((1 - al * (1 - tau)) * u_t_n1 * (1 - ga * o_t_n1.clone()) -
+                   (1 - al) * leak) + I_t1 - (1 - ga) * reVth * o_t_n1.clone()
+        o_t1_n1 = SpikeAct_extended.apply(u_t1_n1 - Vth)
+        return u_t1_n1, o_t1_n1
+
+    def _initialize_params(self, **kwargs):
+        self.mid_gate_mode = True
+        self.tau.copy_(
+            torch.tensor(-math.log(1 / kwargs['param'][0] - 1),
+                         dtype=torch.float,
+                         device=self.tau.device))
+        self.Vth.copy_(
+            torch.tensor(-math.log(1 / kwargs['param'][1] - 1),
+                         dtype=torch.float,
+                         device=self.Vth.device))
+        self.reVth.copy_(
+            torch.tensor(-math.log(1 / kwargs['param'][1] - 1),
+                         dtype=torch.float,
+                         device=self.reVth.device))
+
+        self.leak.copy_(
+            -math.log(1 / kwargs['param'][2] - 1) *
+            torch.ones(self.T, dtype=torch.float, device=self.leak.device))
+        self.conduct.copy_(
+            -math.log(1 / kwargs['param'][3] - 1) *
+            torch.ones(self.T, dtype=torch.float, device=self.conduct.device))
+
+
+class KLIFNode(BaseNode_Torch):
+    """
+    KLIF: An optimized spiking neuron unit for tuning surrogate gradient slope and membrane potential
+    https://arxiv.org/abs/2302.09238
+    """
+
+    def __init__(self,
+                 threshold=1.0,
+                 step=4,
+                 layer_by_layer=True,
+                 tau=2.,
+                 act_fun=Sigmoid_Grad,
+                 mem_detach=False,
+                 *args,
+                 **kwargs):
+        super().__init__(threshold=threshold,
+                         step=step,
+                         layer_by_layer=layer_by_layer,
+                         mem_detach=mem_detach)
+        self.tau = tau
+        if isinstance(act_fun, str):
+            act_fun = eval(act_fun)
+        self.act_fun = act_fun()
+        self.k = nn.Parameter(torch.tensor(1.0), requires_grad=True)
+
+    def integral(self, inputs):
+        self.mem = self.mem + (inputs - self.mem) / self.tau
+
+        # klif
+        self.mem *= self.k
+        self.mem = nn.ReLU(self.mem)
+
+    def calc_spike(self):
+        self.spike = self.act_fun(self.mem - self.threshold)
+        self.mem = self.mem * (1 - self.spike.detach())
