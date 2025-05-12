c5b46395cb67b90ff9e2d4166855f2a919ef5868
casia_wsl <fenglinghao2022@ia.ac.cn>
2025-05-12 22:08:13 +0800

    add firing rate hook for test

---
+++ cls/hooks/firing_rate.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+import torch
+import argparse
+from omegaconf import OmegaConf
+from cls.models.utils.node import LIFNode
+
+
+neuron_outputs = []
+
+
+def load_model(model_config):
+    if model_config.name == "qkformer":
+        from cls.models.static.qkformer_imagenet import qkformer_imagenet
+
+        model = qkformer_imagenet()
+    elif model_config.name == "spikformer":
+        from cls.models.static.spikformer_imagenet import spikformer_imagenet
+
+        model = spikformer_imagenet()
+    else:
+        raise ValueError("Invaild model name")
+
+    return model
+
+
+def record_output_lif(module, input, output):
+    neuron_outputs.append(output)
+
+
+def cal_firing_rate():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True)
+    args = parser.parse_args()
+    config = OmegaConf.load(args.config)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    ckpt_path = config.ckpt_path
+
+    model = load_model(config.model)
+    model.load_state_dict(
+        torch.load(ckpt_path, map_location="cpu", weights_only=False)["state_dict"],
+        strict=False,
+    )
+    model = model.to(device)
+    model.eval()
+
+    for name, submodule in model.named_modules():
+        if isinstance(submodule, LIFNode):
+            print(name)
